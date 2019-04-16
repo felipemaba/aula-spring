51c9648b144c37b3556934bcd32ccb1188eeab5c
Felipe <felipemaba_@hotmail.com>
2019-04-15 23:07:32 -0300

    Criacao de Entity e Ajuste do Properties

---
+++ src/main/java/com/example/aulasspring/api/entities/User.java
@@ -0,0 +1,81 @@
+package com.example.aulasspring.api.entities;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String login;
+	
+	private String password;
+	
+	private Date creationDate;
+	
+	private boolean isActive;
+	
+	public User(Long id, String login, String password, Date creationDate, boolean isActive) {
+		super();
+		this.id = id;
+		this.login = login;
+		this.password = password;
+		this.creationDate = creationDate;
+		this.isActive = isActive;
+	}
+	
+	public User() {
+		
+	};
+
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Date getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+
+	public boolean isActive() {
+		return isActive;
+	}
+
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+
+}
---
+++ src/main/java/com/example/aulasspring/api/enums/Roles.java
@@ -0,0 +1,6 @@
+package com.example.aulasspring.api.enums;
+
+public enum Roles {
+	ROLE_ADMIN,
+	ROLE_USER
+}
