e89886fb581866ecd6528732c3bc994c4f3abed6
Felipe <felipemaba_@hotmail.com>
2019-04-15 21:32:12 -0300

    commit inicial

---
+++ src/main/java/com/example/aulasspring/api/AulasSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.aulasspring.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulasSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulasSpringApplication.class, args);
+	}
+
+}
